c13b893cf6568476362c10a756eb08f2e8b5cb35
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-08-10 12:18:32 -0300

    update

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "db_name" {
+    description = "nome do meu banco de dados"
+    type = "string"
+}
+
+variable "db_password" {
+    description = "password do meu banco de dados"
+    type = "string"
+}
+
+variable "db_username" {
+    description = "username do meu banco de dados"
+    type = "string"
+}
